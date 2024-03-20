ab1313bffd1091808281301bcec19b828ae94426
elmir01 <elmir.mikayilli@gmail.com>
2024-03-20 15:54:47 +0400

    first commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -73,97 +73,100 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Center(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Log in',
-              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
             ),
-          )),
-          SizedBox(
-            height: 30,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(19.0),
-            child: MaterialTextField(
-              keyboardType: TextInputType.emailAddress,
-              hint: 'Email',
-              textInputAction: TextInputAction.next,
-              prefixIcon: const Icon(Icons.email_outlined),
-              controller: _emailTextController,
-              validator: FormValidation.emailTextField,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(19.0),
-            child: MaterialTextField(
-              keyboardType: TextInputType.emailAddress,
-              hint: 'Password',
-              textInputAction: TextInputAction.done,
-              obscureText: !_passwordVisible,
-              // theme: FilledOrOutlinedTextTheme(
-              //   // fillColor: Colors.green.withAlpha(50),
-              //   radius: 12,
-              // ),
-              prefixIcon: Icon(Icons.lock),
-              suffixIcon: IconButton(
-                icon: Icon(
-                  _passwordVisible ? Icons.visibility : Icons.visibility_off,
-                ),
-                onPressed: () {
-                  setState(() {
-                    _passwordVisible = !_passwordVisible;
-                  });
-                },
-              ),
-              controller: _passwordTextController,
-              validator: FormValidation.requiredTextField,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    _signIn();
-                  },
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                )),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Don\'t have an account?',
-              style: TextStyle(fontWeight: FontWeight.w600),
-            ),
-          ),
-          TextButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => RegsisterPage()));
-              },
+            Center(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Sign Up',
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15),
-              ))
-        ],
+                'Log in',
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
+              ),
+            )),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(19.0),
+              child: MaterialTextField(
+                keyboardType: TextInputType.emailAddress,
+                hint: 'Email',
+                textInputAction: TextInputAction.next,
+                prefixIcon: const Icon(Icons.email_outlined),
+                controller: _emailTextController,
+                validator: FormValidation.emailTextField,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(19.0),
+              child: MaterialTextField(
+                keyboardType: TextInputType.emailAddress,
+                hint: 'Password',
+                textInputAction: TextInputAction.done,
+                obscureText: !_passwordVisible,
+                // theme: FilledOrOutlinedTextTheme(
+                //   // fillColor: Colors.green.withAlpha(50),
+                //   radius: 12,
+                // ),
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _passwordVisible ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                ),
+                controller: _passwordTextController,
+                validator: FormValidation.requiredTextField,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _signIn();
+                    },
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Don\'t have an account?',
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RegsisterPage()));
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15),
+                ))
+          ],
+        ),
       ),
     );
   }
